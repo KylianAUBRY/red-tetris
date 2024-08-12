5a7db8294e3b718bcc72c6fc24b3a1c15f043b3f
kyaubry <aubry.kylian973@gmail.com>
2024-08-12 19:10:50 +0200

    adding piece

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,78 @@
 import { ShowBoard } from "./components/plateau";
+
 import React, { useEffect, useRef, useState } from 'react';
 
 function App () {
+  const subtractionOfDeltaTime = 50;
   const rows = 20;
 	const cols = 10;
-  // const [grid, setGrid] = useState (Array.from({ length: rows }, () => Array(cols).fill(null)));
-  // const gridRef = useRef(grid);
 
-  // useEffect(async () => {
-  //   // if (grid)
-  //   //   ;
-  // }, [grid]);
+  const [grid, setGrid] = useState (Array.from({ length: rows }, () => Array(cols).fill(null)));
+  grid[19][9] = 'red';
+	grid[19][8] = 'red';
+	grid[19][7] = 'blue';
+  grid[15][0] = 'blue';
 
+  const [deltaTime, setDeltaTime] = useState(1000); // temps entre chaque changement de ligne en ms
+  const deltaTimeRef = useRef(deltaTime);
+
+  const [piece, setPiece] = useState({
+    x: 0,
+    y: 0,
+    shape: [
+      [null, 'blue', null],
+      ['blue', 'blue', 'blue'],
+      [null, null, null],
+    ],
+  })
+
+  useEffect(() => {
+    deltaTimeRef.current = deltaTime; // Met à jour la référence à chaque changement de deltaTime
+  }, [deltaTime]);
+
+  useEffect(() => {
+    const decreaseInterval = setInterval(() => {
+      setDeltaTime(prevDeltaTime => Math.max(100, prevDeltaTime - subtractionOfDeltaTime)); 
+    }, 1000);
+    return () => clearInterval(decreaseInterval); 
+  }, []);
+
+  function checkPiece(x, y) {
+    var tmpPiece = {...piece};
+    
+    tmpPiece.x += x;
+    tmpPiece.y += y;
+
+    for (var tmpY = 0; tmpY < 3; tmpY++) {
+      for (var tmpX = 0; tmpX < 3; tmpX++) {
+        if (tmpPiece.y + tmpY >= rows || (tmpPiece.shape[tmpY][tmpX] && grid[tmpPiece.y + tmpY][tmpPiece.x + tmpX])) {
+          
+          return 1;
+        }
+      }
+    }
+    return 0;
+  }
+
+
+
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (checkPiece(0, 1) == 1)
+        return ;
+      setPiece(prevPiece => ({
+        ...prevPiece,
+        y: prevPiece.y + 1
+      }));
+    }, deltaTimeRef.current);
+
+    return () => clearInterval(interval);
+  }, [deltaTime]);
   
   return (
     <div>
-      <ShowBoard />
+      <ShowBoard grid={grid} piece={piece}/>
     </div>
   );
 }
--- src/components/plateau.jsx
+++ src/components/plateau.jsx
@@ -1,27 +1,27 @@
-export function ShowBoard () {
-	const rows = 20;
-	const cols = 10;
-	const grid = Array.from({ length: rows }, () => Array(cols).fill(null));
-
-	grid[19][9] = 'red';
-	grid[19][8] = 'red';
-	grid[19][7] = 'blue';
-	grid[5][5] = 'green';
+export function ShowBoard ({grid, piece}) {
 
 	return (
 		<div className="game-box">
 			<div className="tetris-grid">
 				{grid.map((row, rowIndex) =>
-					row.map((cell, colIndex) => (
-						<div
-						  key={`${rowIndex}-${colIndex}`}
-						  className={`tetris-cell`}
-						  style={{ '--piece-color': cell}}
-						>
-							{cell && <div className="inner-square"></div>}
-						</div>
-					))
-        		)}
+					row.map((cell, colIndex) => {
+						const tmpY = rowIndex - piece.y;
+						const tmpX = colIndex - piece.x;
+						var pieceCell = null;
+						if (tmpY >= 0 && tmpX >= 0 && tmpY < 3 && tmpX < 3) {
+							pieceCell = piece.shape[tmpY][tmpX];
+						}
+						return (
+							<div
+							key={`${rowIndex}-${colIndex}`}
+							className={`tetris-cell`}
+							style={{ '--piece-color': pieceCell || cell}}
+							>
+								{(pieceCell || cell) && <div className="inner-square"></div>}
+							</div> 
+						)
+					})
+					)}
       		</div>
     	</div>
   	);
