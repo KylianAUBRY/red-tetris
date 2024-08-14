8bd8d52d37c6b6bcb87dc6c3be2b3862753f990a
kodokaii <kodokai.featheur@gmail.com>
2024-08-14 18:29:59 +0200

    gitignore

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,11 @@
-import globals from 'globals';
-import pluginJs from '@eslint/js';
-import pluginReact from 'eslint-plugin-react';
-import eslintConfigPrettier from 'eslint-config-prettier';
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+
 
 export default [
-	{
-		files: ['**/*.{js,mjs,cjs,jsx}'],
-		languageOptions: { globals: globals.browser },
-		rules: {
-			'nonblock-statement-body-position': ['error', 'below'],
-		},
-		settings: {
-			react: {
-				version: 'detect',
-			},
-		},
-	},
-	pluginJs.configs.recommended,
-	pluginReact.configs.flat.recommended,
-	eslintConfigPrettier,
+  {files: ["**/*.{js,mjs,cjs,jsx}"]},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
 ];
