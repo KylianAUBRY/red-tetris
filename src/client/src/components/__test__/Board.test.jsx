66f7cda19238a28c51df9599c396a42b24d99f12
kodokaii <kodokai.featheur@gmail.com>
2024-10-19 15:06:13 +0200

    migrate to vitest

--- src/client/src/components/__test__/Board.test.jsx
+++ src/client/src/components/__test__/Board.test.jsx
@@ -1,9 +1,11 @@
+import {expect, describe, beforeEach, it} from 'vitest';
 import Board from '../Board.jsx'
 import React from 'react';
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
 import { HEIGHT, WIDTH } from '../../../../constants.js';
+
 
 const mockStore = configureStore([]);
 
